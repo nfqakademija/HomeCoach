25cc65fbd1bb81e82dc006c3e9c0d9929cacd574
Mantas Zambacevicius <zambacevicius.mantas@gmail.com>
2016-03-23 15:28:25 +0200

    Added gulp tasks, npm config, improved file structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,50 +1,41 @@
 'use strict';
 
-var gulp    = require('gulp');
-var sass    = require('gulp-sass');
-var concat  = require('gulp-concat');
-var uglify  = require('gulp-uglify');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 var dir = {
     assets: './app/Resources/',
     dist: './web/',
-    bower: './bower_components/',
-    bootstrapJS: './bower_components/bootstrap-sass/assets/javascripts/bootstrap/'
+    node : './node_modules/'
 };
 
-gulp.task('sass', function() {
-    gulp.src(dir.assets + 'style/main.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(concat('style.css'))
+gulp.task('styles', function() {
+    gulp.src(dir.assets + 'style/*.css')
         .pipe(gulp.dest(dir.dist + 'css'));
 });
 
-gulp.task('scripts', function() {
-    gulp.src([
-            dir.bower + 'jquery/dist/jquery.min.js',
-            // Bootstrap JS modules
-            //dir.bootstrapJS + 'transition.js',
-            //...
-            // Main JS file
-            dir.assets + 'scripts/main.js'
-        ])
-        .pipe(concat('script.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(dir.dist + 'js'));
+gulp.task('scripts',function()
+{
+    gulp.src(dir.assets + 'scripts/*.js')
+        .pipe(gulp.dest(dir.dist +'js'));
 });
 
-gulp.task('images', function() {
-    gulp.src([
-            dir.assets + 'images/**'
-        ])
-        .pipe(gulp.dest(dir.dist + 'images'));
+gulp.task('assets',function()
+{
+    gulp.src(dir.node + 'angular/angular.min.js')
+        .pipe(gulp.dest(dir.dist + 'assets'));
+    gulp.src(dir.node +'bootstrap/dist/css/bootstrap.min.css')
+        .pipe(gulp.dest(dir.dist +'assets'));
+    gulp.src(dir.node + 'bootstrap/dist/js/bootstrap.min.js')
+        .pipe(gulp.dest(dir.dist +'assets'));
+    gulp.src(dir.node + 'jquery/dist/jquery.min.js')
+        .pipe(gulp.dest(dir.dist + 'assets'));
 });
 
-gulp.task('fonts', function() {
-    gulp.src([
-        dir.bower + 'bootstrap-sass/assets/fonts/**'
-        ])
-        .pipe(gulp.dest(dir.dist + 'fonts'));
-});
+gulp.task('default', ['styles', 'scripts', 'assets']);
 
-gulp.task('default', ['sass', 'scripts', 'fonts', 'images']);
+
+
+
