4b646cf137b4899ab115b544524d301e225e4d06
ngreed <n3mirting4s@yahoo.com>
2016-05-04 00:15:35 +0300

    forming the idea of graph futher

--- app/Resources/scripts/weightGraph.js
+++ app/Resources/scripts/weightGraph.js
@@ -1,21 +1,20 @@
 var barChartData = {
+    //TODO pakeisti i datas kada buvo padaryti svorio irasai 
     labels: ["January", "February", "March", "April", "May", "June", "July"],
+    //TODO kiekvienam svorio irasui sukurti atskira dataseto irasa ir kiekvienam priskirti workouta (pagal tai ir uzvadinti "label". skirtingiems labeliams uzdeti skirtingas spalvas
     datasets: [{
-        label: 'Dataset 1',
+        label: 'Vandamo rytmetinis',
         backgroundColor: "rgba(151,187,205,0.5)",
-        data: [1, 4, 5, 2, 10],
+        //TODO reiksme nustatyti pagal svorio vidurki, kuris buvo kai buvo aktyvi sita programa
+        data: [5],
         borderColor: 'white',
-        borderWidth: 2
+        borderWidth: 0,
     }, {
-        label: 'Dataset 2',
-        backgroundColor: "rgba(151,187,205,0.5)",
-        data: [1, 4, 5, 2, 10],
-        borderColor: 'white',
-        borderWidth: 2
-    }, {
+        //TODO sita "line" grafika isvis istrinti, jei naudosim ta metoda, kuri aprasiau auksciau
         type: 'line',
-        label: 'Dataset 3',
+        label: 'Svoris (kg)',
         backgroundColor: "rgba(220,220,220,0.5)",
+        //TODO sudeti masyva su svorio irasais
         data: [1, 4, 5, 2, 10],
     }, ]
 
